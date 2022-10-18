4003b31b5a41995613cde7026975b8d94b1680f7
choijun0 <cjy00050005@gmail.com>
2022-10-18 13:48:14 +0900

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 최준영 on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
